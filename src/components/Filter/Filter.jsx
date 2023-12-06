b81b88f68b6aafb2407a51b91ea93a328487a59a
Krzysztof Kryczka <kw.kryczka@gmail.com>
2023-12-06 17:50:32 +0100

    added material-ui styling to Filter Contact

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { filterContact } from 'redux/contacts/slice/filterSlice';
-
-import css from './Filter.module.css';
+import Paper from '@mui/material/Paper';
+import InputBase from '@mui/material/InputBase';
+import IconButton from '@mui/material/IconButton';
+import SearchIcon from '@mui/icons-material/Search';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
 
 export const Filter = () => {
   const dispatch = useDispatch();
@@ -12,15 +16,50 @@ export const Filter = () => {
   };
 
   return (
-    <div className={css.filter}>
-      <h2>Find contacts by name</h2>
-      <input
-        type="text"
-        name="filter"
-        value={filterValue}
-        onChange={onFilterChange}
-        className={css.filter__input}
-      ></input>
-    </div>
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        padding: '20px',
+      }}
+    >
+      <Typography
+        variant="h4"
+        sx={{
+          textAlign: 'center',
+          fontWeight: 'bold',
+          fontSize: 'h4.fontSize',
+          color: '#53a7eb',
+        }}
+      >
+        {`Find contacts by name`}
+      </Typography>
+      <Paper
+        component="form"
+        sx={{
+          p: '2px 4px',
+          display: 'flex',
+          alignItems: 'center',
+          width: 400,
+          boxShadow:
+            '0px 2px 1px -1px rgb(25 118 210), 0px 1px 1px 0px rgb(25 118 210), 0px 1px 3px 0px rgb(25 118 210)',
+        }}
+      >
+        <InputBase
+          sx={{ ml: 1, flex: 1 }}
+          placeholder="Find contacts by name"
+          inputProps={{ 'aria-label': 'find contacts by name' }}
+          type="text"
+          name="filter"
+          value={filterValue}
+          title="Enter name"
+          onChange={onFilterChange}
+        />
+        <IconButton type="button" sx={{ p: '10px' }} aria-label="search">
+          <SearchIcon />
+        </IconButton>
+      </Paper>
+    </Box>
   );
 };
